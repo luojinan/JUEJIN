982a7c59a1009551f88990316aebfd89f7acd96c
kingan <1017386624@qq.com>
2018-07-14 16:17:33 +0800

    移动端配置准备

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'	//移动端点击插件--引入
+
+//移动端样式准备
+import 'styles/reset.css'
+import 'styles/border.css'
 
 Vue.config.productionTip = false
+fastclick.attach(document.body)		//移动端点击插件--配置
 
 /* eslint-disable no-new */
 new Vue({
