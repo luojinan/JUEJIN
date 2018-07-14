176da9f566bf17822d9083ae236b3f04065b2b41
kingan <1017386624@qq.com>
2018-07-14 23:14:43 +0800

    完成首页页面布局

--- src/pages/home/components/Homepage.vue
+++ src/pages/home/components/Homepage.vue
@@ -1,7 +1,58 @@
 <template>
 	<div class="homepage">
-	  <div>
-	    homepage
+	  <div class="homepage-recommend">
+	  	<div class="homepage-recommend__title">
+	    	<span class="iconfont">&#xe627;</span>
+	    	热门推荐
+	    	<span class="iconfont">&#xe653;</span>
+	    	<span class="iconfont">&#xe64d;</span>
+	    </div>
+	    <div class="homepage-recommend__item">
+	    	<div class="homepage-recommend__item--info">
+	    		<div class="homepage-recommend__item--title">这是标题</div>
+	    		<div class="homepage-recommend__item--desc">
+	    		<span class="iconfont">&#xe627;</span>10
+	    		<span class="iconfont">&#xe62f;</span>渔人码头
+	    		<span class="iconfont">&#xe627;</span>40分钟前
+	    		</div>
+	    	</div>
+	    	<img class="homepage-recommend__item--img" src="https://user-gold-cdn.xitu.io/2018/7/13/16491cbd31f9640f?imageView2/1/w/120/h/120/q/85/format/webp/interlace/1" />
+	    </div>
+	    <div class="homepage-recommend__item">
+	    	<div class="homepage-recommend__item--info">
+	    		<div class="homepage-recommend__item--title">这是标题</div>
+	    		<div class="homepage-recommend__item--desc">
+	    		<span class="iconfont">&#xe627;</span>10
+	    		<span class="iconfont">&#xe62f;</span>渔人码头
+	    		<span class="iconfont">&#xe627;</span>40分钟前
+	    		</div>
+	    	</div>
+	    	<img class="homepage-recommend__item--img" src="https://user-gold-cdn.xitu.io/2018/7/13/16491cbd31f9640f?imageView2/1/w/120/h/120/q/85/format/webp/interlace/1" />
+	    </div>
+	    <div class="homepage-recommend__item">
+	    	<div class="homepage-recommend__item--info">
+	    		<div class="homepage-recommend__item--title">这是标题</div>
+	    		<div class="homepage-recommend__item--desc">
+	    		<span class="iconfont">&#xe627;</span>10
+	    		<span class="iconfont">&#xe62f;</span>渔人码头
+	    		<span class="iconfont">&#xe627;</span>40分钟前
+	    		</div>
+	    	</div>
+	    	<img class="homepage-recommend__item--img" src="https://user-gold-cdn.xitu.io/2018/7/13/16491cbd31f9640f?imageView2/1/w/120/h/120/q/85/format/webp/interlace/1" />
+	    </div>
+	  </div>
+	  <div class="homepage-list">
+	  	<div class="homepage-recommend__item">
+	    	<div class="homepage-recommend__item--info">
+	    		<div class="homepage-recommend__item--title">这是标题</div>
+	    		<div class="homepage-recommend__item--desc">
+	    		<span class="iconfont">&#xe627;</span>10
+	    		<span class="iconfont">&#xe62f;</span>渔人码头
+	    		<span class="iconfont">&#xe627;</span>40分钟前
+	    		</div>
+	    	</div>
+	    	<img class="homepage-recommend__item--img" src="https://user-gold-cdn.xitu.io/2018/7/13/16491cbd31f9640f?imageView2/1/w/120/h/120/q/85/format/webp/interlace/1" />
+	    </div>
 	  </div>
 	</div>
 </template>
@@ -20,6 +71,44 @@ export default {
 		right: 0;
 		
 		height: 50rem;
-		background-color: #ccc;
+		background-color: #eee;
+	}
+	.homepage-recommend{
+		border-top: solid .2rem #eee;
+	}
+	.homepage-recommend__title{
+		display: flex;
+		height: .8rem;
+		line-height: .8rem;
+		background-color: #fff;
+		border-bottom: solid .01rem #eee;
+	}
+	.homepage-recommend__item{
+		height: 2rem;
+		background-color: #fff;
+		display: flex;
+		border-bottom: solid .03rem #eee;
+	}
+	.homepage-recommend__item--info{
+		flex: 1;
+		margin-top: .4rem;
+		padding-left: .2rem;
+	}
+	.homepage-recommend__item--title{
+		font-size: .44rem;
+	}
+	.homepage-recommend__item--desc{
+		color: #ccc;
+		margin-top: .2rem;
+		font-size: .12rem;
+	}
+	.homepage-recommend__item--img{
+		width: 1.2rem;
+		height: 1.2rem;
+		margin-top: .4rem;
+		margin-right: .2rem;
+	}
+	.homepage-list{
+		margin-top:.2rem;
 	}
 </style>
