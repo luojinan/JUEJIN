cf8b94234732f814c08c1b3fa2d5db1db15fe796
kingan <1017386624@qq.com>
2018-07-16 21:34:23 +0800

    完成find页面头部布局和路由

--- src/pages/find/components/FindHeader.vue
+++ src/pages/find/components/FindHeader.vue
@@ -1,6 +1,37 @@
 <template>
-  <div>
-    发现页头部
+  <div class="findheader">
+    <router-link 
+    	tag="div"
+    	to="/"
+    	class="findheader-item"
+    >
+    	<span class="iconfont findheader-item__icon">&#xe603;</span>
+    	本周最热
+    </router-link>
+    <router-link 
+    	tag="div"
+    	to="/"
+    	class="findheader-item"
+    >
+    	<span class="iconfont findheader-item__icon">&#xe67f;</span>
+    	收藏集
+    </router-link>
+    <router-link 
+    	tag="div"
+    	to="/"
+    	class="findheader-item"
+    >
+    	<span class="iconfont findheader-item__icon">&#xe631;</span>
+    	线下活动
+    </router-link>
+    <router-link 
+    	tag="div"
+    	to="/"
+    	class="findheader-item"
+    >
+    	<span class="iconfont findheader-item__icon">&#xe628;</span>
+    	专栏
+    </router-link>
   </div>
 </template>
 
@@ -11,4 +42,22 @@ export default {
 </script>
 
 <style>
+	.findheader{
+		display: flex;
+		height: 2rem;
+		text-align: center;
+		border-bottom: solid .1rem #eee; 
+	}
+	.findheader-item{
+		flex: 1;
+
+		display: flex;
+		flex-direction: column;/*垂直排列布局*/
+		justify-content: center;
+	}
+	.findheader-item__icon{
+		font-size: .6rem;
+		color: #1296db;
+		margin-bottom: .2rem;
+	}
 </style>
